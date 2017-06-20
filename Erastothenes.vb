8ee932f5a30c28f88f034b14aaeb9be4002c975c
sidjayisaok <sidjay@Kilauea.localdomain>
2017-06-19 21:09:34 -0400

    minor refactoring

--- Erastothenes.vb
+++ Erastothenes.vb
@@ -1,7 +1,8 @@
 Sub Erastothenes()
 
 'variables
-Dim x, y, z, a, c As Integer
+Dim x, a as Integer
+Dim y, z, c As Long
 
 'improve efficiency
 With Application
@@ -13,27 +14,29 @@ With Application
 x = 4
 a = Application.InputBox("Enter a number, but keep it under 1000 for brevity")
 
+'clean the sheet
 Cells.Delete
 
 'renders display to squares
 With Cells
     .RowHeight = 5 * x + 5
-    .ColumnWidth = x
+    .ColumnWidth = x + 5
     .WrapText = True
     .Font.Color = RGB(0, 0, 0)
-    .Borders.Color = RGB(0, 0, 0)
 End With
 
 'Sieve of Eratosthenes used to generate graph
 For y = 1 To a
 For z = 1 To a
 For c = 2 To a
+    'matrix manipulation
     Cells(y, z) = (y * a) + (z - a)
+        'determine if number is not prime
         If Cells(y, z) Mod c = 0 And Cells(y, z) <> c Then
             With Cells(y, z)
-        .Interior.Color = RGB(0, 0, 0)
-        .Font.Color = RGB(255, 255, 255)
-        End With
+                .Interior.Color = RGB(0, 0, 0)
+                .Font.Color = RGB(255, 255, 255)
+            End With
         End If
 Next c
 Next z
